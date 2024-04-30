496b143edfe9cdc71be6ab07ab93304a5ca8e5fc
haeun104 <haeun104@gmail.com>
2024-04-30 21:02:22 +0200

    Created user menu dropdown

--- components/navbar/UserMenu.tsx
+++ components/navbar/UserMenu.tsx
@@ -3,24 +3,49 @@
 import { UserData } from "@/types";
 import avatar from "@/public/images/avatar.jpg";
 import Image from "next/image";
+import MenuItem from "./MenuItem";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface UserMenuProps {
   currentUser: UserData | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
+  const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
+
   if (currentUser) {
     return (
-      <>
-      <div className="flex h-full gap-2 items-center cursor-pointer">
-        <div className="text-main-gray font-semibold group-hover:text-white uppercase hidden lg:flex">
+      <div className="flex h-full gap-2 items-center cursor-pointer relative">
+        <div
+          className="text-main-gray font-semibold group-hover:text-white uppercase hidden lg:flex"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           {`${currentUser.firstName} ${currentUser.lastName}`}
         </div>
-        <div className="rounded-full overflow-hidden">
+        <div
+          className="rounded-full overflow-hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           <Image src={avatar} alt="avatar" height={30} width={30} />
         </div>
+        {isOpen && (
+          <div className="absolute right-0 top-14 flex flex-col gap-4 bg-white px-4 py-2 rounded-lg w-[300px]">
+            <span className="bg-white h-[16px] w-[16px] absolute right-[10px] -top-[4px] rotate-45"></span>
+            <MenuItem
+              title="My Reservations"
+              style="text-main-gray"
+              onClick={() => router.push("/reservations")}
+            />
+            <MenuItem
+              title="My Profile"
+              style="text-main-gray"
+              onClick={() => router.push("/profile")}
+            />
+          </div>
+        )}
       </div>
-      </>
     );
   }
 };
