3215d62a1105d713a48f2a6fac9a331963fda520
haeun104 <haeun104@gmail.com>
2024-05-06 21:40:37 +0200

    Created account and reservation pages

---
+++ app/reservations/page.tsx
@@ -0,0 +1,11 @@
+import Container from "@/components/Container";
+
+const ReservationsPage = () => {
+  return (
+    <Container>
+      <div>My Reservations Page</div>
+    </Container>
+  );
+};
+
+export default ReservationsPage;
