3f84b59be1292474cb7a82cf578f77b141c6d37f
haeun104 <haeun104@gmail.com>
2024-05-11 00:49:15 +0200

    Corrected date format and added menu dropdown

--- components/reservation/ReservationItem.tsx
+++ components/reservation/ReservationItem.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { Room } from "@prisma/client";
 import Button from "../Button";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { format } from "date-fns";
+import { isAfter } from "date-fns";
+import { BsThreeDots } from "react-icons/bs";
 
 interface ReservationItemProps {
   id: string;
@@ -18,7 +19,12 @@ const ReservationItem: React.FC<ReservationItemProps> = ({
   endDate,
   totalPrice,
 }) => {
-  const [isReservationPassed, setIsReservationPassed] = useState(false);
+  const [isReservationPassed, setIsReservationPassed] = useState<boolean>();
+  const [menuHidden, setMenuHidden] = useState(true);
+
+  useEffect(() => {
+    setIsReservationPassed(isAfter(new Date(), startDate));
+  }, [startDate]);
 
   const formatDate = useMemo(() => {
     const start = format(startDate, "MM/dd/yyyy");
@@ -36,29 +42,58 @@ const ReservationItem: React.FC<ReservationItemProps> = ({
       </div>
       <div className="flex-1 flex flex-col justify-between">
         <h3 className="font-bold">room name</h3>
-        <p className="text-sm">{`from ${formatDate.start} to ${formatDate.end}`}</p>
         <span className="font-bold">{totalPrice} PLN</span>
+        <p className="text-sm">{`from ${formatDate.start} to ${formatDate.end}`}</p>
+        {isReservationPassed && <span className="text-sm">Finished</span>}
       </div>
       {!isReservationPassed ? (
-        <div className="flex flex-col gap-2">
+        <div className="hidden sm:flex sm:flex-col sm:gap-2">
           <Button
             title="Edit reservation"
             style="bg-accent-light-green text-sm font-normal"
           />
           <Button
             title="Cancel reservation"
-            style="bg-main-gray text-sm font-normal"
+            style="bg-[#DDDDDD] text-sm font-normal"
           />
         </div>
       ) : (
-        <div className="flex flex-col gap-2">
-          <div className="text-sm">Finished</div>
+        <div className="hidden sm:flex sm:flex-col sm:gap-2 min-w-[160px]">
           <Button
             title="Write a review"
             style="bg-accent-light-green text-sm font-normal"
           />
+          <Button
+            title="Reserve again"
+            style="bg-accent-red text-sm font-normal"
+          />
         </div>
       )}
+      <div className="sm:hidden relative">
+        <div
+          className="cursor-pointer"
+          onClick={() => setMenuHidden(!menuHidden)}
+        >
+          <BsThreeDots />
+        </div>
+        <div
+          className={`text-sm text-right absolute top-6 right-0 ${
+            menuHidden ? "hidden" : ""
+          } bg-white border-solid border-[1px] border-[#EEEEEE] rounded-lg px-4 py-2`}
+        >
+          {!isReservationPassed ? (
+            <>
+              <div>Edit reservation</div>
+              <div>Cancel reservation</div>
+            </>
+          ) : (
+            <>
+              <div>Write a review</div>
+              <div>Reserve again</div>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
