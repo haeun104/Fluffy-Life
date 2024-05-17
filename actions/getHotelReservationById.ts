4771b9cd24722d4f890a9e594ebe4ba26b6a32e9
haeun104 <haeun104@gmail.com>
2024-05-17 19:08:54 +0200

    Updated reservation detail page

---
+++ actions/getHotelReservationById.ts
@@ -0,0 +1,29 @@
+import prisma from "@/prisma/prismadb";
+import { getFormattedDate } from "@/util";
+
+export default async function getHotelReservationById(id: string) {
+  try {
+    const reservation = await prisma.hotelReservation.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        pet: true,
+        room: true,
+        user: true,
+      },
+    });
+
+    if (!reservation) {
+      return null;
+    }
+
+    return {
+      ...reservation,
+      startDate: getFormattedDate(reservation.startDate),
+      endDate: getFormattedDate(reservation.endDate),
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
--- app/reservations/[reservationId]/page.tsx
+++ app/reservations/[reservationId]/page.tsx
@@ -1,19 +1,57 @@
+import getHotelReservationById from "@/actions/getHotelReservationById";
 import Container from "@/components/Container";
+import Image from "next/image";
 
 interface ReservationDetailParams {
   reservationId: string;
 }
 
-const ReservationDetailPage = ({
+const ReservationDetailPage = async ({
   params,
 }: {
   params: ReservationDetailParams;
 }) => {
-  return (
-    <Container>
-      <div>Reservation {params.reservationId}</div>
-    </Container>
-  );
+  const reservation = await getHotelReservationById(params.reservationId);
+
+  if (reservation) {
+    const { imageUrl, roomType, description } = reservation.room;
+
+    return (
+      <Container>
+        <div className="py-10 max-w-[600px] mx-auto">
+          <h2 className="font-bold text-main-teal">Reservation Details</h2>
+          <div className="mt-4">
+            <h3 className="font-bold text-accent-light-green mb-4">Room</h3>
+            <div className="flex flex-col gap-2">
+              <div className="rounded-md overflow-hidden h-[200px]">
+                <Image src={imageUrl} alt={roomType} width={350} height={200} />
+              </div>
+              <div className="flex justify-between">
+                <h4>Check-in</h4>
+                <div>{reservation.startDate}</div>
+              </div>
+              <div className="flex justify-between">
+                <h4>Check-out</h4>
+                <div>{reservation.endDate}</div>
+              </div>
+              <div className="flex justify-between">
+                <h4>Total Price</h4>
+                <div>{reservation.totalPrice} PLN</div>
+              </div>
+              <div className="flex flex-col">
+                <h4>Room Information</h4>
+                <div>{description} </div>
+              </div>
+              <div className="flex flex-col">
+                <h4>Additional Information</h4>
+                <div>...</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    );
+  }
 };
 
 export default ReservationDetailPage;
