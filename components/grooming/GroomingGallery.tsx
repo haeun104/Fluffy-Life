a2504723c34b44370031c5cce1d628ac5719f186
haeun104 <haeun104@gmail.com>
2024-05-30 18:51:45 +0200

    Added animation for grooming gallery

--- components/grooming/GroomingGallery.tsx
+++ components/grooming/GroomingGallery.tsx
@@ -8,13 +8,13 @@ const GroomingGallery = () => {
     <>
       <h3 className="text-accent-light-green font-bold mt-10">Gallery</h3>
       <div className="flex flex-col sm:flex-row">
-        <div className="max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden">
+        <div className="opacity-0 animate-fade-in-1 max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden">
           <Image src={bath} alt="grooming bath" />
         </div>
-        <div className="max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden">
+        <div className="opacity-0 animate-fade-in-2 max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden">
           <Image src={haircut} alt="grooming haircut" />
         </div>
-        <div className="max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden">
+        <div className="opacity-0 animate-fade-in-3 max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden">
           <Image src={brush} alt="grooming brush" />
         </div>
       </div>
