47c858515e856cd80b0ab81a5760740f43f7077a
haeun104 <haeun104@gmail.com>
2024-05-30 22:16:35 +0200

    Deleted uncessary logic from server side

--- app/hotel/page.tsx
+++ app/hotel/page.tsx
@@ -1,27 +1,17 @@
 import RoomList from "@/components/hotel/RoomList";
 import Container from "@/components/Container";
-import getAvailableRooms, {
-  RoomSearchParams,
-} from "@/actions/getAvailableRooms";
+import { RoomSearchParams } from "@/actions/getAvailableRooms";
 
 const HotelPage = async ({
   searchParams,
 }: {
   searchParams: RoomSearchParams;
 }) => {
-  const rooms = await getAvailableRooms(searchParams);
-
-  if (!rooms) {
-    return null;
-  }
-
   return (
     <Container>
       <div className="py-10 max-w-[350px] sm:max-w-[800px] flex flex-col gap-4 mx-auto">
         <h2 className="text-main-teal text-lg font-bold">Rooms</h2>
-        <RoomList
-          searchParams={searchParams}
-        />
+        <RoomList searchParams={searchParams} />
       </div>
     </Container>
   );
