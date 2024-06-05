4af9207944556e4c12feb437ddd6642d989e0740
haeun104 <haeun104@gmail.com>
2024-06-05 11:20:12 +0200

    Added the logic for loading and hiding previous reservations

--- components/reservation/HotelReservations.tsx
+++ components/reservation/HotelReservations.tsx
@@ -3,7 +3,7 @@
 import { HotelReview, Room } from "@prisma/client";
 import ReservationItem from "./ReservationItem";
 import ReviewRegistrationModal from "../modals/ReviewRegistrationModal";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { RoomData, UserData } from "@/types";
 import useHotelReviewModal from "@/hooks/useReviewRegistrationModal";
 
@@ -36,6 +36,8 @@ const HotelReservations: React.FC<HotelReservationsProps> = ({
   const [reviewToUpdate, setReviewToUpdate] = useState<HotelReview | null>(
     null
   );
+  const [visibleReservations, setVisibleReservations] = useState(3);
+  const loadMoreRef = useRef<HTMLDivElement | null>(null);
 
   const hotelReviewModal = useHotelReviewModal();
 
@@ -68,6 +70,22 @@ const HotelReservations: React.FC<HotelReservationsProps> = ({
     hotelReviewModal.onOpen();
   };
 
+  const loadMoreReservations = () => {
+    if (hotelReservations) {
+      setVisibleReservations((prev) =>
+        Math.min(prev + 3, hotelReservations.length)
+      );
+    }
+    if (loadMoreRef.current) {
+      loadMoreRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const hidePreviousReservation = () => {
+    setVisibleReservations(3);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <>
       <div className="">
@@ -78,25 +96,46 @@ const HotelReservations: React.FC<HotelReservationsProps> = ({
           <div className="mt-8">There is no reservations</div>
         ) : (
           <div className="flex flex-col gap-4 mt-4">
-            {hotelReservations.map((reservation) => {
-              const { imageUrl, roomType, id } = reservation.room;
-              return (
-                <ReservationItem
-                  key={reservation.id}
-                  id={reservation.id}
-                  startDate={reservation.startDate}
-                  endDate={reservation.endDate}
-                  totalPrice={reservation.totalPrice}
-                  imageUrl={imageUrl}
-                  roomType={roomType}
-                  roomId={id}
-                  existingReview={
-                    checkExistingReview(reservation.id) === null ? false : true
-                  }
-                  openHotelReviewModal={openHotelReviewModal}
-                />
-              );
-            })}
+            {hotelReservations
+              .slice(0, visibleReservations)
+              .map((reservation) => {
+                const { imageUrl, roomType, id } = reservation.room;
+                return (
+                  <ReservationItem
+                    key={reservation.id}
+                    id={reservation.id}
+                    startDate={reservation.startDate}
+                    endDate={reservation.endDate}
+                    totalPrice={reservation.totalPrice}
+                    imageUrl={imageUrl}
+                    roomType={roomType}
+                    roomId={id}
+                    existingReview={
+                      checkExistingReview(reservation.id) === null
+                        ? false
+                        : true
+                    }
+                    openHotelReviewModal={openHotelReviewModal}
+                  />
+                );
+              })}
+          </div>
+        )}
+        {hotelReservations && (
+          <div className="border-[1px] border-[#EEEEEE] rounded-md p-2 shadow-md flex gap-4 mt-4 justify-center hover:bg-[#EEEEEE]">
+            {visibleReservations < hotelReservations.length ? (
+              <div
+                onClick={loadMoreReservations}
+                className="cursor-pointer"
+                ref={loadMoreRef}
+              >
+                Load previous reservations
+              </div>
+            ) : (
+              <div onClick={hidePreviousReservation} className="cursor-pointer">
+                Hide previous reservations
+              </div>
+            )}
           </div>
         )}
       </div>
