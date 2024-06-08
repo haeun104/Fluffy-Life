4e6b76e84485865e4741565966b854e4482f8e13
haeun104 <haeun104@gmail.com>
2024-06-08 18:22:39 +0200

    Changed initial review layout

--- components/hotel/RoomReviewList.tsx
+++ components/hotel/RoomReviewList.tsx
@@ -5,7 +5,7 @@ import { RoomReview } from "@/types";
 import AverageRating from "../AverageRating";
 import { useCallback, useEffect, useState } from "react";
 import getHotelReviews from "@/actions/getHotelReviews";
-import InfiniteScroll from "react-infinite-scroll-component";
+import useReviewListModal from "@/hooks/useReviewListModal";
 
 export interface RoomReviewProps {
   roomId: string;
@@ -13,33 +13,31 @@ export interface RoomReviewProps {
 
 const RoomReviewList: React.FC<RoomReviewProps> = ({ roomId }) => {
   const [reviews, setReviews] = useState<RoomReview[]>([]);
-  const [hasMore, setHasMore] = useState(true);
-  const [visibleCount, setVisibleCount] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
+  const reviewListModal = useReviewListModal();
 
-  const fetchData = useCallback(async () => {
-    setIsLoading(true);
+  // fetch reviews for initial display
+  const fetchReviews = useCallback(async () => {
     try {
-      const take = 2;
-      const skip = visibleCount * take;
+      const take = 4;
+      const skip = 0;
       const response = await getHotelReviews(roomId, false, skip, take);
       if (response && response.length > 0) {
-        setReviews((prev) => [...prev, ...response]);
-        setVisibleCount((prev) => prev + 1);
+        setReviews(response);
       } else {
-        setHasMore(false);
       }
     } catch (error: any) {
       console.error(error);
-      setHasMore(false);
-    } finally {
-      setIsLoading(false);
     }
-  }, [roomId, visibleCount]);
+  }, [roomId]);
 
   useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+    fetchReviews();
+  }, [fetchReviews]);
+
+  // Open modal for more reviews
+  const handleClickMoreReviews = () => {
+    reviewListModal.onOpen();
+  };
 
   return (
     <>
@@ -47,38 +45,36 @@ const RoomReviewList: React.FC<RoomReviewProps> = ({ roomId }) => {
         <h2 className="font-bold text-lg">Reviews</h2>
         <AverageRating reviews={reviews} />
         <div
-          className={`border-solid border-y-[1px] border-[#EEEEEE] mt-4 flex ${
+          className={`relative border-y-[1px] border-[#EEEEEE] mt-4 flex ${
             reviews.length === 0 && "justify-center items-center h-[200px]"
           }`}
         >
-          {isLoading && reviews.length === 0 ? (
+          {reviews.length === 0 ? (
             <div>Loading...</div>
           ) : reviews.length === 0 ? (
             <div>There is no review registered yet</div>
           ) : (
-            <div className="w-full">
-              <InfiniteScroll
-                dataLength={reviews.length}
-                next={fetchData}
-                hasMore={hasMore}
-                loader={<p>Loading...</p>}
-                className="flex flex-col gap-4 md:flex-row md:flex-wrap md:gap-0"
-              >
-                {reviews.map((review, index) => {
-                  const { name } = review.user;
-                  return (
-                    <RoomReviewItem
-                      key={index}
-                      rating={review.rating}
-                      review={review.review}
-                      createdAt={review.createdAt}
-                      userName={name}
-                    />
-                  );
-                })}
-              </InfiniteScroll>
+            <div className="w-full flex flex-col gap-4 md:flex-row md:flex-wrap md:gap-0 relative">
+              {reviews.map((review, index) => {
+                const { name } = review.user;
+                return (
+                  <RoomReviewItem
+                    key={index}
+                    rating={review.rating}
+                    review={review.review}
+                    createdAt={review.createdAt}
+                    userName={name}
+                  />
+                );
+              })}
+              <div className="absolute inset-x-0 bottom-0 h-[250px] bg-gradient-to-t from-white to-transparent"></div>
             </div>
           )}
+          <div className="absolute z-50 bottom-0 py-6 bg-white w-full text-center">
+            <span className="cursor-pointer" onClick={handleClickMoreReviews}>
+              Show more reviews
+            </span>
+          </div>
         </div>
       </div>
     </>
